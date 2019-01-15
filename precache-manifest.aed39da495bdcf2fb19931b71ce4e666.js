a52e31f01854204ed49680f9b3036b02a8981f78
khalil saleh <kha86lilo@live.com>
2019-01-15 23:35:16 +0300

    Updates

---
+++ precache-manifest.aed39da495bdcf2fb19931b71ce4e666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kha86lilo/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "847cd7a5619e0ad4d8a4",
+    "url": "/kha86lilo/robofriends/static/js/runtime~main.847cd7a5.js"
+  },
+  {
+    "revision": "1e4bd813ae6ac68d6d73",
+    "url": "/kha86lilo/robofriends/static/js/main.1e4bd813.chunk.js"
+  },
+  {
+    "revision": "0ce446782efefcd26b1b",
+    "url": "/kha86lilo/robofriends/static/js/1.0ce44678.chunk.js"
+  },
+  {
+    "revision": "1e4bd813ae6ac68d6d73",
+    "url": "/kha86lilo/robofriends/static/css/main.7c5690a4.chunk.css"
+  },
+  {
+    "revision": "0ce446782efefcd26b1b",
+    "url": "/kha86lilo/robofriends/static/css/1.e909a457.chunk.css"
+  },
+  {
+    "revision": "3d1d3dc10ebf1149d3c5eb9731232150",
+    "url": "/kha86lilo/robofriends/index.html"
+  }
+];
